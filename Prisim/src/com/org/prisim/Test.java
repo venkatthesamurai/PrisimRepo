982ed23faa7b3c2c54528142b22f9d9d83af72b9
venkat <venkatthesamurai@gmail.com>
2018-05-11 19:35:55 +0530

    Second Branch

---
+++ Prisim/src/com/org/prisim/Test.java
@@ -0,0 +1,11 @@
+package com.org.prisim;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Operation O=new Operation();
+		int result=O.add(4, 5);
+		System.out.println(+result);
+	}
+
+}
